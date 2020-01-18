929b8eceebc2bdb10632078a4e1dcedd632ec676
marco-helpme <57805863+marco-helpme@users.noreply.github.com>
2020-01-18 12:11:51 -0800

    arreglando el index

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        relaciones
-      </h1>
-      <h2 class="subtitle">
-        My hunky-dory Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <terms-use></terms-use>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
+import termsUse from './termsUse'
 
 export default {
   components: {
-    Logo
+    termsUse
   }
 }
 </script>
